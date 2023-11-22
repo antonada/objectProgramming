d727441272cccca90a9f475f78df820407d8525d
Anton Grischenko <47002327+antonada@users.noreply.github.com>
2023-11-22 13:07:38 +0100

    lab 4

---
+++ Lesson_3/ex_1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ex_1
+{
+    class Shape
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public virtual void Draw()
+        {
+            Console.WriteLine($"Drawing a generic shape at ({X}, {Y}) with height {Height} and width {Width}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new List<Shape>();
+
+            shapes.Add(new Rectangle(0, 0, 5, 10));
+            shapes.Add(new Triangle(0, 0, 5, 10));
+            shapes.Add(new Circle(0, 5, 10));
+
+            foreach (var shape in shapes)
+            {
+                shape.Draw();
+                Console.WriteLine();
+            }
+
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public Rectangle(int x, int y, int height, int width)
+        {
+            X = x;
+            Y = y;
+            Height = height;
+            Width = width;
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine($"Drawing a rectangle at ({X}, {Y}) with height {Height} and width {Width}");
+        }
+    }
+
+    class Triangle : Shape
+    {
+        public Triangle(int x, int y, int height, int width)
+        {
+            X = x;
+            Y = y;
+            Height = height;
+            Width = width;
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine($"Drawing a Triangle at ({X}, {Y}) with height {Height} and width {Width}");
+        }
+    }
+
+    class Circle : Shape
+    {
+        public Circle(int x, int y, int radius)
+        {
+            X = x;
+            Y = y;
+            Height = radius * 2;
+            Width = radius * 2;
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine($"Drawing a Circle at ({X}, {Y}) with radius {Height / 2}");
+        }
+    }
+}
---
+++ Lesson_3/ex_2/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ex_2
+{
+    abstract class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public string PASEL;
+
+        public void SetFirstName(string firstName)
+        {
+            FirstName = firstName;
+        }
+        public void SetLastName(string lastName)
+        {
+            LastName = lastName;
+        }
+
+        public void SetPasel(string pasel)
+        {
+            PASEL = pasel;
+        }
+
+        public abstract int GetAge();
+        public abstract string GetGender();
+        public abstract string GetEducationInfo();
+        public abstract string GetFullName();
+        public abstract bool CanGoAloneToHome();
+    }
+
+    class Student : Person
+    {
+        public string School;
+        private bool canGoAloneToHome;
+
+        public void SetSchool(string school)
+        {
+            School = school;
+        }
+        public void SetCanGoHomeAlone(bool canGoHomeAlone)
+        {
+            canGoAloneToHome = canGoHomeAlone;
+        }
+        public void ChangeSchool(string newSchool)
+        {
+            School = newSchool;
+        }
+
+        public override int GetAge()
+        {
+            return 18;
+        }
+
+        public override string GetGender()
+        {
+            return PASEL[9] % 2 == 0 ? "Female" : "Male";
+        }
+        public override string GetEducationInfo()
+        {
+            return $"Student at {School}";
+        }
+        public override string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+        public override bool CanGoAloneToHome()
+        {
+            return canGoAloneToHome || GetAge() >= 12;
+        }
+
+        public string Info()
+        {
+            if (GetAge() < 12 && !canGoAloneToHome)
+            {
+                return "Under 12 cannot return alone unless they have permission";
+            }
+            else
+            {
+                return "Student can go home alone";
+            }
+        }
+    }
+
+    class Teacher : Student
+    {
+        private string AcademicTitle;
+        private List<Student> SubordinateStudents;
+
+        public Teacher(string academicTitle)
+        {
+            AcademicTitle = academicTitle;
+            SubordinateStudents = new List <Student>();
+        }
+        public void AddSubordinateStudent(Student student)
+        {
+            SubordinateStudents.Add(student);
+        }
+        public void WhichStudentCanGoHomeAlone()
+        {
+            Console.WriteLine("Students who can go home alone:");
+            foreach (var student in SubordinateStudents)
+            {
+                if (student.CanGoAloneToHome())
+                {
+                    Console.WriteLine($"{student.GetFullName()} - {student.GetGender()} - {student.Info()}");
+                }
+            }
+        }
+        public void PrintTeacherAndStudentsInfo(string dayOfWeek)
+        {
+            Console.WriteLine($"{School} On: {dayOfWeek}");
+            Console.WriteLine($"Teacher: {AcademicTitle} {FirstName} {LastName}");
+            Console.WriteLine("List of students:");
+            int lp = 1;
+            foreach (var student in SubordinateStudents)
+            {
+                Console.WriteLine($"{lp}. {student.GetFullName()} - {student.GetGender()} - {student.Info()}");
+                lp++;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher("Dr.");
+            teacher.SetFirstName("Anton");
+            teacher.SetLastName("Hryshchenko");
+            teacher.SetSchool("School");
+
+            Student student1 = new Student();
+            student1.SetFirstName("Johny");
+            student1.SetLastName("Depp");
+            student1.SetSchool("School");
+            student1.SetCanGoHomeAlone(false);
+
+            Student student2 = new Student();
+            student2.SetFirstName("Robert");
+            student2.SetLastName("Polson");
+            student2.SetSchool("School");
+            student2.SetCanGoHomeAlone(true);
+
+            teacher.AddSubordinateStudent(student1);
+            teacher.AddSubordinateStudent(student2);
+
+            teacher.PrintTeacherAndStudentsInfo("Whensday");
+            teacher.WhichStudentCanGoHomeAlone();
+        }
+    }
+}
